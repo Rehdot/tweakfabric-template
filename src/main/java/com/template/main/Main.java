64e19e2a5fa6fff22c435d5d887a1c2df8aa64fe
Redot <masonjprice05@gmail.com>
2026-08-08 23:31:13 -0500

    Initial commit

---
+++ src/main/java/com/template/main/Main.java
@@ -0,0 +1,12 @@
+package com.template.main;
+
+import redot.tweaksuite.commons.annotation.Entrypoint;
+
+public class Main {
+
+    @Entrypoint
+    public static void run() {
+        System.out.println("Hello, World!");
+    }
+
+}
